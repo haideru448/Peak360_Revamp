6efe86c440470291175c21c2a3467ad99ff4bf36
unknown <haideru448@gmail.com>
2022-04-19 13:52:21 +0500

    Ftp credentials

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -10,7 +10,8 @@ import TextField from '@mui/material/TextField';
 import TimePicker from '@mui/lab/TimePicker';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
-
+import { useFormik, Form, FormikProvider } from 'formik';
+import * as Yup from 'yup';
 import Page from '../components/Page';
 import Iconify from '../components/Iconify';
 import { BlogPostCard, BlogPostsSort, BlogPostsSearch } from '../sections/@dashboard/blog';
@@ -30,7 +31,10 @@ export default function Blog() {
   const [currentClientId, setcurrentClientId] = React.useState(-1);
   const [textFieldState, setTextFieldState] = React.useState(true);
   const [ftpCredentialsState, setFtpCredentialsState] = React.useState(true);
-  const [ftpConfigurations, setFtpConfigurations] = React.useState({ip:"",password:"",userName:""});
+  const [ftpConfigurations, setFtpConfigurations] = React.useState({username:"",password:"",ip:""});
+  const [ftpConfigurationsName, setFtpConfigurationsName] = React.useState("");
+
+
 
 
   const [clientId, setclientId] = React.useState("");
@@ -51,7 +55,8 @@ export default function Blog() {
       setclientId(response.data.client_id[0].client_id)
       setcurrentClientId(response.data.client_id[0].id)
       console.log("The current client id is ", currentClientId)
-      
+   
+      getFTPCredentials()
 
 
     }).catch((err) => {
@@ -62,6 +67,71 @@ export default function Blog() {
     });
 
   }, []);
+
+  const Schema = Yup.object().shape({
+    ip: Yup.string().required('Ip is required'),
+    userName: Yup.string().required('username is required'),
+    password: Yup.string().required('Password is required'),
+  });
+  const formik = useFormik({
+    enableReinitialize: true,
+    initialValues: {
+      ip: ftpConfigurations?.ip ?? '' ,
+      userName: ftpConfigurations?.username ?? '' ,
+      password: ftpConfigurations?.password ?? '',
+      // label: '',
+      // artistName: 'ajdlkfjl',
+      // language: '',
+
+    },
+    validationSchema: Schema,
+    onSubmit: async (data) => {
+
+      try {
+
+        axios.post(`${process.env.REACT_APP_SERVER_URL}/ftp_credentials`, data).then((response) => {
+          // handle success
+          console.log("the axios api response", response);
+          toast.success(response.data.message)
+          setTextFieldState(true)
+  
+        }).catch((err) => {
+          console.error("Due to some Error request failed: ", err);
+  
+  
+        });
+        console.log("the data is",data)
+      
+        console.log("hello")
+      } catch (err) {
+        console.log(err);
+        //
+      }
+    }
+  });
+ 
+  const { errors, touched, values, isSubmitting, handleSubmit, getFieldProps } = formik;
+
+
+
+
+
+function getFTPCredentials()
+{let configurations;
+  axios.get(`${process.env.REACT_APP_SERVER_URL}/ftp_credentials`).then((response) => {
+  
+    // handle success
+    console.log("the axios api response for credentials", response.data.credentials[0])
+    setFtpConfigurations(response.data.credentials[0])
+    
+    console.log("sAVED IN STAT",ftpConfigurations)
+  
+  }).catch((err) => {
+    
+  
+  });
+  
+} 
 
   function handleClick() {
     console.log("in the handlee click")
@@ -82,6 +152,7 @@ export default function Blog() {
   function enableFTPCredentialsField()
   {setFtpCredentialsState(false)
   }
+  
 
 
 
@@ -140,14 +211,32 @@ export default function Blog() {
 
         </center>
        
-
-        <center><TextField disabled={ftpCredentialsState} type="text" style={{ "width": "40%" }} name="ip" id="standard-basic" label="FTP IP" variant="filled" value={ftpConfigurations.ip}  /><br /><br />
-        <TextField disabled={ftpCredentialsState} type="text" style={{ "width": "40%" }} name="userName" id="standard-basic" label="FTP Username" variant="filled" value={ftpConfigurations.userName} /><br /><br />
-        <TextField disabled={ftpCredentialsState} type="password" style={{ "width": "40%" }} name="password" id="standard-basic" label="FTP PASSWORD" variant="filled" value={ftpConfigurations.password} />
+        <FormikProvider value={formik}>
+        <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
+        <center><TextField
+        
+        {...getFieldProps('ip')}
+                  error={Boolean(touched.ip && errors.ip)}
+                  helperText={touched.ip && errors.ip}
+                  disabled={ftpCredentialsState} type="text" style={{ "width": "40%" }} name="ip" id="standard-basic" label="FTP IP" variant="filled"  /><br /><br />
+        <TextField
+        {...getFieldProps('userName')}
+        error={Boolean(touched.userName && errors.userName)}
+        helperText={touched.userName && errors.userName}
+        disabled={ftpCredentialsState} type="text" style={{ "width": "40%" }} name="userName" id="standard-basic" label="FTP Username" variant="filled" /><br /><br />
+        <TextField
+          {...getFieldProps('password')}
+          error={Boolean(touched.password && errors.password)}
+          helperText={touched.password && errors.password}
+        
+        disabled={ftpCredentialsState} type="password" style={{ "width": "40%" }} name="password" id="standard-basic" label="FTP PASSWORD" variant="filled"  />
 
         
         <br /><br />
-        <Box sx={{display:"flex",alignItems:"center"}}>  <Button sx={{marginLeft:"auto"}}  variant="contained" >Save</Button><br /><br /><br />
+        <Box sx={{display:"flex",alignItems:"center"}}>  <Button
+        type="submit"
+        sx={{marginLeft:"auto"}}  variant="contained" >Save</Button><br /><br /><br />
+       
          &nbsp;&nbsp;&nbsp; <Button sx={{marginRight:"auto"}} variant="contained" onClick={()=>{enableFTPCredentialsField()}}>Edit</Button></Box>
         <Toaster
   position="top-center"
@@ -155,7 +244,8 @@ export default function Blog() {
 />
 
         </center>
-       
+        </Form>
+        </FormikProvider>
 
 
 
