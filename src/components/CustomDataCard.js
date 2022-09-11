2082062bf5bf86e60b93c01e34faadabcd2a2e04
Usman Haider <haideru448@gmail.com>
2022-09-11 14:21:27 +0500

    Amount and Purchased Items change

--- src/components/CustomDataCard.js
+++ src/components/CustomDataCard.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable radix */
 import * as React from 'react';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
@@ -14,148 +16,141 @@ let PaymentAmount;
 let purchasedItems;
 let payments;
 const monthNames = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
+  "July", "August", "September", "October", "November", "December"
 ];
 const columns = [
-    { id: 'name', label: 'Name', minWidth: 170 },
-    { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
-    {
-        id: 'population',
-        label: 'Population',
-        minWidth: 170,
-        align: 'right',
-        format: (value) => value.toLocaleString('en-US'),
-    },
-    {
-        id: 'size',
-        label: 'Size\u00a0(km\u00b2)',
-        minWidth: 170,
-        align: 'right',
-        format: (value) => value.toLocaleString('en-US'),
-    },
-    {
-        id: 'density',
-        label: 'Density',
-        minWidth: 170,
-        align: 'right',
-        format: (value) => value.toFixed(2),
-    },
+  { id: 'name', label: 'Name', minWidth: 170 },
+  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+  {
+    id: 'population',
+    label: 'Population',
+    minWidth: 170,
+    align: 'right',
+    format: (value) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'size',
+    label: 'Size\u00a0(km\u00b2)',
+    minWidth: 170,
+    align: 'right',
+    format: (value) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'density',
+    label: 'Density',
+    minWidth: 170,
+    align: 'right',
+    format: (value) => value.toFixed(2),
+  },
 ];
 
 function createData(name, code, population, size) {
-    const density = population / size;
-    return { name, code, population, size, density };
+  const density = population / size;
+  return { name, code, population, size, density };
 }
 
 
 export default function StickyHeadTable(props) {
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-    };
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
 
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(+event.target.value);
-        setPage(0);
-    };
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
-    return (
-        <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-            <TableContainer sx={{ maxHeight: 440 }}>
-                <Table stickyHeader aria-label="sticky table">
-                    <TableHead>
-                        <TableRow>
+  return (
+    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
 
-                            <TableCell
+              <TableCell
 
-                            >
-                                Sales Description
-                            </TableCell>
-                            <TableCell
+              >
+                Sales Description
+              </TableCell>
+              <TableCell
 
-                            >
-                                Sales Cost
-                            </TableCell>
-                            <TableCell
-
-
-                            >
-                                Sale Time
-                            </TableCell>
-
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {props.salesData
-                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                            .map((row) => (
-                                
+              >
+                Sales Cost
+              </TableCell>
+              <TableCell
 
 
-                                <TableRow hover role="checkbox" tabIndex={-1} key={row.Id}>
-                                    <TableCell>
-                                    <span style={{display:"none"}}>{PaymentAmount=0}</span>
-                                    <span style={{display:"none"}}>{purchasedItems=""}</span>
+              >
+                Sale Time
+              </TableCell>
 
-                                        
-                                        {
-                                        row.PurchasedItems.forEach((purchased)=>
-                                        {purchasedItems+=purchased.Description}
-                                        )
-                                        }
-                                        {purchasedItems}
-
-                                    </TableCell>
-                                    <TableCell >
-                                        
-                
-                                        {
-                                        row.PurchasedItems.forEach((payment)=>
-                                        {PaymentAmount+=Number(payment.UnitPrice)}
-                                        )
-                                        }
-                                        {
-                                        Number(PaymentAmount).toFixed(2)
-                                        }
-                                    
-
-
-                                    </TableCell>
-                                    <TableCell >
-                                        {row.SaleDateTime.split("-")[2].slice(0, 2)}&nbsp;
-                                        {monthNames[parseInt(row.SaleDateTime.split("-")[1], 10) - 1]}&nbsp;&nbsp;
-                                        {row.SaleDateTime.split("-")[0]
-
-                                        },&nbsp;
-                                        {row.SaleDateTime.split("T")[1].slice(0, 8)
-
-
-                                        } &nbsp;
-                                        
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {props.salesData
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => (
 
 
 
-                                    </TableCell>
+                <TableRow hover role="checkbox" tabIndex={-1} key={row.Id}>
+                  <TableCell>
+                    <span style={{ display: "none" }}>{PaymentAmount = 0}</span>
 
 
 
-                                </TableRow>
+                    {row.PurchasedItems.map((data) => (<span>{data.Description}<br /></span>))}
 
-                            ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <TablePagination
-                rowsPerPageOptions={[10, 25, 100]}
-                component="div"
-                count={props.salesData.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-            />
-        </Paper >
-    );
+                    {/* {purchasedItems} */}
+
+                  </TableCell>
+                  <TableCell >
+
+
+
+
+                    {row.PurchasedItems.map((payment) => (<span>{(parseInt(payment.TotalAmount) - parseInt(payment.TaxAmount)).toFixed(2)}<br /></span>))}
+
+
+
+
+                  </TableCell>
+                  <TableCell >
+                    {row.SaleDateTime.split("-")[2].slice(0, 2)}&nbsp;
+                    {monthNames[parseInt(row.SaleDateTime.split("-")[1], 10) - 1]}&nbsp;&nbsp;
+                    {row.SaleDateTime.split("-")[0]
+
+                    },&nbsp;
+                    {row.SaleDateTime.split("T")[1].slice(0, 8)
+
+
+                    } &nbsp;
+
+
+
+
+                  </TableCell>
+
+
+
+                </TableRow>
+
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={props.salesData.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper >
+  );
 }
